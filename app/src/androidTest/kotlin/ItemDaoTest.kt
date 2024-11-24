39a472813868c49b2e5601f19c7e9c0348f88c9d
Syifaar <syifaarifah29@gmail.com>
2024-11-24 14:10:29 +0700

    Mengedit entity item

--- app/src/androidTest/kotlin/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -85,5 +85,13 @@ class ItemDaoTest {
         assertTrue(allItems.isEmpty())
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoGetItem_returnsItemFromDB() = runBlocking {
+        addOneItemToDb()
+        val item = itemDao.getItem(1)
+        assertEquals(item.first(), item1)
+    }
+
 }
 
